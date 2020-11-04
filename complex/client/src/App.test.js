d903cef05cec537cb4b53adebec08be55c209437
BoozieBoi <michael.heinecke.1337@gmail.com>
2020-11-04 19:43:53 +0100

    Replacing files with snapshot from multi-docker project

--- complex/client/src/App.test.js
+++ complex/client/src/App.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders without crashing', () => {});
+test('renders learn react link', () => {});
